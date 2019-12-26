b8b86fe907cc4f54c08b01dd921478849f683d73
meitian <meitian3611@163.com>
2019-12-26 18:47:20 +0800

    antd sass 搭建

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
+/** @format */
 
+import React from "react"
 
-const App = ()=>{
-  return(
+const App = () => {
+  return (
     <div>
       <h1>App</h1>
     </div>
   )
 }
-
 
 export default App
