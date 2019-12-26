1b3f24914411a2593543ff8850757c4d17ecb936
meitian <meitian3611@163.com>
2019-12-26 19:29:55 +0800

    项目界面搭建

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 /** @format */
 
 import React from "react"
+import BaseLayout from "./layouts/BaseLayout"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={BaseLayout}></Route>
+      </Switch>
+    </Router>
   )
 }
 
