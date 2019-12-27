3f95b358780760222122b2a94d29d32263566037
meitian <meitian3611@163.com>
2019-12-27 10:32:31 +0800

    登录页面样式设置

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -7,39 +7,53 @@ import { Row, Col, Form, Icon, Input, Button } from "antd"
 import "./index.scss"
 
 class Login extends React.PureComponent {
+  handleSubmit = e => {
+    e.preventDefault()
+  }
   render() {
+    // const { getFieldDecorator } = this.props.form
     return (
       <div className="page-login">
-        <Row>
+        <Row className="page-login_row" type="flex" align="middle">
           <Col span={8} offset={8}>
-            <Form>
-              <Form.Item>
-                <Input
-                  prefix={
-                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  placeholder="Username"
-                />
-              </Form.Item>
-              <Form.Item>
-                <Input
-                  prefix={
-                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  type="password"
-                  placeholder="Password"
-                />
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  className="login-form-button"
-                >
-                  登录
-                </Button>
-              </Form.Item>
-            </Form>
+            <div>
+              <Form onSubmit={this.handleSubmit} className="login-form">
+                <Form.Item>
+                  <h1 className="login-form_title">Administrator</h1>
+                </Form.Item>
+                <Form.Item>
+                  <Input
+                    prefix={
+                      <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                    }
+                    placeholder="Username"
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Input
+                    prefix={
+                      <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                    }
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="login-form_button"
+                  >
+                    登录
+                  </Button>
+                </Form.Item>
+                <Form.Item>
+                  <Button type="Default" className="login-form_button">
+                    注册
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
           </Col>
         </Row>
       </div>
